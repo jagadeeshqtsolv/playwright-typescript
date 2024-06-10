da7dc0df4926a24ff3796706728754ccaa239d09
jagadeesh <jagadeesh@qtsolv.com>
2024-06-10 13:05:16 +0530

    added summary report screenshot

---
+++ generateAllureReportScreenshot.js
@@ -0,0 +1,14 @@
+const { chromium } = require('playwright');
+const path = require('path');
+
+(async () => {
+    const browser = await chromium.launch({ headless: true });
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    const filePath = path.resolve(__dirname, './allure-report/index.html');
+    await page.goto(`file://${filePath}`);
+    await page.screenshot({ path: './allure-report/summary-screenshot.png' });
+    await browser.close();
+})();
+
+
